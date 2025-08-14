b5350eed0e15ab0db0f694cbad7d35c45c528ee1
Asqar1477 <rayimkulovasqar5@gmail.com>
2025-08-14 16:57:43 +0500

    inital commit

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 import asyncio
 
-# .env fayldan o'zgaruvchilarni yuklash
+# .env yuklash
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 RAILWAY_URL = os.getenv("RAILWAY_URL")
 
-# Flask app yaratamiz
+# Flask app
 app = Flask(__name__)
 
-# Telegram bot application
+# Telegram bot
 application = Application.builder().token(BOT_TOKEN).build()
 
 # /start komandasi
@@ -24,15 +24,16 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(update.message.text)
 
-# Handlerlarni qo'shish
+# Handlerlarni qo‘shish
 application.add_handler(CommandHandler("start", start))
 application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
 # Webhook endpoint
 @app.route(f"/{BOT_TOKEN}", methods=["POST"])
-def webhook():
-    update = Update.de_json(request.get_json(force=True), application.bot)
-    application.update_queue.put_nowait(update)
+async def webhook():
+    data = request.get_json(force=True)
+    update = Update.de_json(data, application.bot)
+    await application.process_update(update)  # shu update’ni ishlatadi
     return "OK", 200
 
 # Home route
@@ -40,13 +41,11 @@ def webhook():
 def home():
     return "Bot ishlayapti 🚀", 200
 
+async def main():
+    # Webhook o‘rnatish
+    await application.bot.set_webhook(f"{RAILWAY_URL}/{BOT_TOKEN}")
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8443))
-
-    async def set_webhook():
-        await application.bot.set_webhook(f"{RAILWAY_URL}/{BOT_TOKEN}")
-
-
-    asyncio.run(set_webhook())  # Webhook o‘rnatish
-
+    asyncio.run(main())
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
